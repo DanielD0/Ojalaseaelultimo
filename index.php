c946fcaabec9548777922f3ce622923bd3c14968
CristianOrtega <desarrollo18@warriorslabs.com>
2020-08-04 12:14:00 -0500

    Agrego el index.php

--- index.php
+++ index.php
@@ -114,28 +114,28 @@
             <!-- Navbar brand -->
             <a class="navbar-brand" href="#">
                 <a class="nav-link" href=""><img src="img/qq.ico" id="img-ico">
-                </a>
+            </a>
 
-                <!-- Collapse button -->
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+            <!-- Collapse button -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
 
-                <!-- Collapsible content -->
-                <div class="collapse navbar-collapse" id="basicExampleNav">
+            <!-- Collapsible content -->
+            <div class="collapse navbar-collapse" id="basicExampleNav">
 
-                    <!-- Links -->
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Servicios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">SPEM</a>
-                        </li>
-                    </ul>
-                    <!-- Links -->
-                </div>
-                <!-- Collapsible content -->
+                <!-- Links -->
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">SPEM</a>
+                    </li>
+                </ul>
+                <!-- Links -->
+            </div>
+            <!-- Collapsible content -->
         </nav>
         <div class="view">
             <img src="img/E.jpg" class="img-fluid">
