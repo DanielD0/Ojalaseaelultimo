f1a51972e0c06cf7a9a363546568d0d3d3a991d6
DanielD0 <37585619+DanielD0@users.noreply.github.com>
2020-08-04 00:43:01 -0500

    creacion del footer

---
+++ footer.php
@@ -0,0 +1,178 @@
+<section id="footer">
+                <footer class="page-footer font-small stylish-color-dark pt-4">
+
+                    <!-- Footer Links -->
+                    <div class="container-fluid text-center text-md-left w-100">
+
+                        <!-- Grid row -->
+                        <div class="row mx-auto">
+
+                            <!-- Grid column -->
+                            <div class="col-md-4 mx-auto">
+
+                                <!-- Content -->
+                                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Contactanos</h5>
+                                <p>Pulsa alguna de las direcciones para saber como llegar.</p>
+
+                            </div>
+                            <!-- Grid column -->
+
+                            <hr class="clearfix w-100 d-md-none">
+
+                            <!-- Grid column -->
+                            <div class="col-md-2 mx-auto">
+
+                                <!-- Links -->
+                                <h5 class="font-weight-bold text-uppercase mt-3 mb-4 text-center">Corporativo
+                                </h5>
+
+                                <ul class="list-unstyled text-justify">
+                                    <br>
+                                    <hr>
+                                    <li>
+                                        <a href="https://www.google.com.mx/maps/place/Cr%C3%A1ter+171,+Jardines+del+Pedregal,+%C3%81lvaro+Obreg%C3%B3n,+01900+Ciudad+de+M%C3%A9xico,+CDMX/@19.3251861,-99.2067617,17z/data=!3m1!4b1!4m5!3m4!1s0x85cdfff1c79a6fe1:0xabe9c60f98cb1c92!8m2!3d19.3251861!4d-99.204573">
+                Cráter 171, Colonia Jardines del Pedregal, C.P. 01900, Ciudad de México, Tel: (55) 1206 5090</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            <!-- Grid column -->
+
+                            <hr class="clearfix w-100 d-md-none">
+
+                            <!-- Grid column -->
+                            <div class="col-md-2 mx-auto">
+
+                                <!-- Links -->
+                                <h5 class="font-weight-bold text-uppercase mt-3 mb-4 text-justify">Sucursal del Valle</h5>
+
+                                <ul class="list-unstyled text-justify">
+                                    <hr>
+                                    <li>
+                                        <a href="https://www.google.com.mx/maps/place/SPEM+Soluciones/@19.3670227,-99.1791,17z/data=!4m8!1m2!2m1!1s103,+San+Francisco+1838,+Acacias,+Benito+Ju%C3%A1rez,+03100+Ciudad+de+M%C3%A9xico,+CDMX!3m4!1s0x85d1ff1ae91f44d3:0x7ce4c18f58da7ecd!8m2!3d19.3670699!4d-99.1769605">
+                San Francisco 1838, Int. 103 y 104, Colonia Actipan del Valle, C.P. 03100, Ciudad de México, Tel: (55) 7587 8220</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            <!-- Grid column -->
+
+                            <hr class="clearfix w-100 d-md-none">
+
+                            <!-- Grid column -->
+                            <div class="col-md-2 mx-auto">
+
+                                <!-- Links -->
+                                <h5 class="font-weight-bold text-uppercase mt-3 mb-4 text-center">Representación en USA</h5>
+
+                                <ul class="list-unstyled text-justify">
+                                    <hr>
+                                    <li>
+                                        <a href="https://www.google.com/maps/place/2313+Lockhill+Selma+Rd+%23103,+San+Antonio,+TX+78231,+EE.+UU./@29.543689,-98.5317068,17z/data=!3m1!4b1!4m5!3m4!1s0x865c60f5e006429b:0xb68c781e4d803159!8m2!3d29.543689!4d-98.5295181">2313 Lockhill Selma, Suite 103, C.P. 78230, San Antonio, Texas. USA, Tel:(210) 504 4662</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            <!-- Grid column -->
+
+                        </div>
+                        <!-- Grid row -->
+
+                    </div>
+                    <!-- Footer Links -->
+
+                    <hr>
+
+                    <!-- Call to action -->
+                    <ul class="list-unstyled list-inline text-center py-2">
+                        <li class="list-inline-item">
+                            <h5 class="mb-1">Contactanos!!!</h5>
+                        </li>
+                    </ul>
+                    <div class="modal fade" id="modalContactForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header text-center">
+                                    <h4 class="modal-title font-weight-bolder">Llena los siguientes datos y pronto nos pondremos en contacto contigo</h4>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                  </button>
+                                </div>
+                                <div class="modal-body mx-3">
+                                    <div class="md-form mb-5">
+                                        <i class="fas fa-user prefix grey-text"></i>
+                                        <input type="text" id="nombre" class="form-control validate">
+                                        <label data-error="wrong" data-success="right" for="form34">Nombre</label>
+                                    </div>
+
+                                    <div class="md-form mb-5">
+                                        <i class="fas fa-envelope prefix grey-text"></i>
+                                        <input type="email" id="email" class="form-control validate">
+                                        <label data-error="wrong" data-success="right" for="form29">Email</label>
+                                    </div>
+
+                                    <div class="md-form mb-5">
+                                        <i class="fas fa-tag prefix grey-text"></i>
+                                        <input type="text" id="asunto" class="form-control validate">
+                                        <label data-error="wrong" data-success="right" for="form32">Asunto</label>
+                                    </div>
+
+                                    <div class="md-form">
+                                        <i class="fas fa-pencil prefix grey-text"></i>
+                                        <textarea type="text" id="mensaje" class="md-textarea form-control" rows="4"></textarea>
+                                        <label data-error="wrong" data-success="right" for="form8">Tu Mensaje</label>
+                                    </div>
+
+                                </div>
+                                <div class="modal-footer d-flex justify-content-center">
+                                    <button class="btn blue-gradient" id="submit">Enviar <i class="fas fa-paper-plane-o ml-1"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="text-center">
+                        <a href="" class="btn aqua-gradient btn-rounded mb-4" data-toggle="modal" data-target="#modalContactForm">Pulsa Aqui</a>
+                    </div>
+                    <!-- Call to action -->
+
+                    <hr>
+
+                    <!-- Social buttons -->
+                    <ul class="list-unstyled list-inline text-center">
+                        <li class="list-inline-item">
+                            <a class="btn-floating btn-fb mx-1">
+                                <i class="fab fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn-floating btn-tw mx-1">
+                                <i class="fab fa-twitter-square"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn-floating btn-gplus mx-1">
+                                <i class="fab fa-google-plus-square"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn-floating btn-li mx-1">
+                                <i class="fab fa-linkedin"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn-floating btn-dribbble mx-1">
+                                <i class="fas fa-map"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- Social buttons -->
+
+                    <!-- Copyright -->
+                    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                        <a href="#"> SPEM.com</a>
+                    </div>
+                    <!-- Copyright -->
+
+                </footer>
+</section>
